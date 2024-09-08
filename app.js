64535068e7536b136990eca0cf236415a21d3af3
christiansanchez-SE <christiansanchez714@gmail.com>
2024-09-07 21:36:42 -0400

    added the logic behind the password generator

---
+++ app.js
@@ -0,0 +1,36 @@
+// Selects the input element where the generated password will be displayed
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "ABCDDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|}{[]></-=";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword() {
+    const password = passwordBox.value;
+    if (password) {
+        navigator.clipboard.writeText(password).then(() => {
+            alert("Password copied to clipboard!");
+        }).catch(err => {
+            alert("Failed to copy password: " + err);
+        });
+    } else {
+        alert("No password to copy!");
+    }
+}
